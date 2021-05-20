b10ba8a06a8d69d0bcb4b4bc67929029343c1509
tejas181995 <tejasdev95@gmail.com>
2021-05-20 17:47:57 +0530

    ADD. login page UI  designed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,33 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppLogoComponent } from './components/app-logo/app-logo.component';
+import { LoginComponent } from './components/login/login.component';
+import { FlexLayoutModule } from "@angular/flex-layout";
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+ 
 
 @NgModule({
   declarations: [
     AppComponent,
-    AppLogoComponent
+    AppLogoComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MatFormFieldModule,
+    MatIconModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatButtonModule,
+    FormsModule,
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
